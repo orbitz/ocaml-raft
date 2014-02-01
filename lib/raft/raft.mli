6d08382b12c6c620a6610d1529b0f63eb1101788
orbitz <orbitz@gmail.com>
2014-02-01 15:49:10 +0100

    Add initial interface

---
+++ lib/raft/raft.mli
@@ -0,0 +1,40 @@
+module type LOG = sig
+  type elt
+  type t
+
+  (* Not required to be pure *)
+  val append : elt -> t -> t option
+end
+
+module type SERVER = sig
+  type t
+
+  val equal : t -> t -> bool
+end
+
+module Make : functor (Server : SERVER) -> functor (Log : LOG) -> sig
+  type 'a t
+
+  val create            : Server.t list -> [ `Follower ] t
+
+  val servers           : 'a t -> Server.t list
+  val set_servers       : Server.t list -> 'a t -> 'a t
+
+  val current_term      : 'a t -> int
+  val voted_for         : 'a t -> Server.t option
+
+  val log               : 'a t -> Log.t
+
+  val leader            : 'a t -> Server.t option
+
+  val next_index        : [ `Leader ] t -> (Server.t * int) list
+  val match_index       : [ `Leader ] t -> (Server.t * int) list
+
+  val heartbeat_timeout : [ `Follower] t -> [ `Candidate ] t
+
+  val election_timeout  : [ `Candidate ] t -> [ `Candidate ] t
+  val receive_vote      : [ `Candidate ] t -> [ `Candidate | `Leader ] t
+
+  val append            : Log.t -> 'a t -> 'a t
+
+end
